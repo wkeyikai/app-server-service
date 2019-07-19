7f4618a0bf293e8ea1088a3e20a4ba83de0c2dbc
Aaron <aaron.tw@landteam.tech>
2019-07-19 12:03:46 +0800

    新增server static file

--- project/server/index.js
+++ project/server/index.js
@@ -3,7 +3,7 @@ const app = express()
 const api = require('./api')
 
 api.build(app)
-
+app.use('/static', express.static(__dirname +'/static'))
 app.get('/', function (req, res) {
   //res.send('Hello World')
   res.sendFile(__dirname + '/view/index.html')
